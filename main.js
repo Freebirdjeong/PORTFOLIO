918a5343ca2d854435b0444a1cd5f1d5d5516597
Freebirdjeong <avs5_20@naver.com>
2021-08-24 17:03:37 +0900

    JS: Navbar menu toggle + Project UI tweak

--- main.js
+++ main.js
@@ -20,7 +20,6 @@ document.addEventListener('scroll', () => {
     }
 });
 
-
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -29,9 +28,18 @@ navbarMenu.addEventListener('click', (event) => {
     if (link == null) {
         return;
     }
-    console.log(event.target.dataset.link);
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 });
+
+
+// Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+});
+
+
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');

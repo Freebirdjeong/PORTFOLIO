d4bdf4ef2911c625fe7dd4850e0561db1515ef6c
Freebirdjeong <avs5_20@naver.com>
2021-08-24 16:31:59 +0900

    JS: remove avtive from nav

--- main.js
+++ main.js
@@ -65,7 +65,6 @@ arrowUp.addEventListener('click', () => {
 
 
 // Projects
-
 const workBtnContainer = document.querySelector('.work__categories');
 const projectContainer = document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.project');
@@ -74,6 +73,11 @@ workBtnContainer.addEventListener('click', (e) => {
     if (filter == null) {
         return;
     }
+    // Remove selection from the previous item and select the new one
+    const active = document.querySelector('.category__btn.selected');
+    active.classList.remove('selected');
+    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+    target.classList.add('selected');
     projectContainer.classList.add('anim-out')
     setTimeout(() => {
         projects.forEach((project) => {
